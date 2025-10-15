e4bb9156fa881bbbe089360300bddd8bd3e5505a
Rehmer <alexander.rehmer@gmail.com>
2025-10-15 09:52:34 +0200

    clean slate

--- python_loader/cv2/config-3.12.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Make sure the package directory itself is searched first for the .pyd
-PYTHON_EXTENSIONS_PATHS = [
-    os.path.dirname(os.path.abspath(__file__)),
-] + PYTHON_EXTENSIONS_PATHS
