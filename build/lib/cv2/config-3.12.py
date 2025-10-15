eedaf7df098d43d18d5679b8504333c99bd34046
Rehmer <alexander.rehmer@gmail.com>
2025-10-15 16:21:05 +0200

    Version 0.3

--- build/lib/cv2/config-3.12.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# __file__ is the path of the current file
-current_dir = Path(__file__).parent
-pyd_dir = current_dir / 'lib' / 'python3' / 'Release'
-
-PYTHON_EXTENSIONS_PATHS = [
-    str(pyd_dir)
-] + PYTHON_EXTENSIONS_PATHS
