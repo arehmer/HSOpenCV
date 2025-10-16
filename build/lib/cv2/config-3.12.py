dc69a5959876342fc8dbd93ec6d69b0bca98aa0c
Rehmer <alexander.rehmer@gmail.com>
2025-10-16 11:34:21 +0200

    removed build artefacts

--- build/lib/cv2/config-3.12.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# __file__ is the path of the current file
-current_dir = Path(__file__).parent
-pyd_dir = current_dir / 'lib' 
-
-PYTHON_EXTENSIONS_PATHS = [
-    str(pyd_dir)
-] + PYTHON_EXTENSIONS_PATHS
