ab50be61af5118101d12b3499e220ad5caab706e
arehmer <alexander.rehmer@gmail.com>
2026-01-28 11:27:14 +0100

    Added Linux binaries

---
+++ Linux/python_loader/cv2/config-3.13.py
@@ -0,0 +1,3 @@
+PYTHON_EXTENSIONS_PATHS = [
+    '/home/heimann/OpenCV_build/lib/python3'
+] + PYTHON_EXTENSIONS_PATHS
---
+++ Linux/python_loader/cv2/config.py
@@ -0,0 +1,5 @@
+import os
+
+BINARIES_PATHS = [
+    '/home/heimann/OpenCV_build/lib'
+] + BINARIES_PATHS
